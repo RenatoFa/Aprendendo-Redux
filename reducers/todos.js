758348e6a56f62705ad3d6ebc700543c873e1b81
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-06-25 14:56:19 -0300

    Reducers

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,5 +1,17 @@
 // Criação do reducer : ouvir minha action criaada
 // So uma função 
-export default  function todos (){
+export default  function todos (state=[],action){
+// action={type='ADD_TODO , text: 'Fazer cafe''}
+switch(action.type){
 
+    case 'ADD_TODO':
+        {
+            return [...state , {id: Math.random(), text: action.text }]
+        }
+
+
+    default: 
+            return state;
+
+}
 }
