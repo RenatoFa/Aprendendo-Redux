2ce4e4ad11f63a089f1d841d2249e23f83d814fa
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-06-25 01:05:13 -0300

    Reducers

--- actions/todos.js
+++ actions/todos.js
@@ -1,5 +1,6 @@
 export function addTodo(text){
  // disparar minhas action para o reducer ouvir
+ // sempre é uma função 
  return{
      type: 'ADD_TODO',
      text,
---
+++ reducers/todos.js
@@ -0,0 +1,5 @@
+// Criação do reducer : ouvir minha action criaada
+// So uma função 
+export default  function todos (){
+
+}
